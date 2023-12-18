924a3108903fbcc226f46aaaaa61bc5112727864
Karan Dixit <97967796+dixitk941@users.noreply.github.com>
2023-12-18 23:08:17 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const toggleBtn = document.querySelector('.toggle-btn');
-const navLinks = document.querySelector('.nav-links');
-
-toggleBtn.addEventListener('click', () => {
-  navLinks.classList.toggle('active');
-});
